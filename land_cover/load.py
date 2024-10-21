062688263591f824ee88fb1c0e470b356d77996a
ekcomputer <ethan_kyzivat@fas.harvard.edu>
2024-10-21 17:42:30 -0400

    Loading from joined files

--- land_cover/load.py
+++ land_cover/load.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import pandas as pd
 import geopandas as gpd
 
 def loadEfflux():
     return gpd.read_file('/Volumes/metis/ABOVE3/LAKESHAPE/effluxlakes.shp')
+
+
+def loadEffluxShp():
+    gdf = gpd.read_file('/Volumes/metis/ABOVE3/Tom/Selected_PLD_Lakes_2024-10-21/EffluxLakes_selected_PLDLakes_2024-10-11.shp')
+    df = pd.read_excel('/Volumes/metis/ABOVE3/Tom/PrelimLakeMatchupData_2024-10-21.xlsx')
+    return gdf.merge(df, on='lake_id')
+
+# gdf = loadEffluxShp()
+# pass
