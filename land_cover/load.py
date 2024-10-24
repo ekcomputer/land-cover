bee889e9d321a12c4d319adb1b2aa730bb3b7d67
ekcomputer <ethan_kyzivat@fas.harvard.edu>
2024-10-24 15:00:00 -0400

    Join in WBD and plotting fxns

--- land_cover/load.py
+++ land_cover/load.py
@@ -7,9 +7,19 @@ def loadEfflux():
 
 
 def loadEffluxShp():
-    gdf = gpd.read_file('/Volumes/metis/ABOVE3/Tom/Selected_PLD_Lakes_2024-10-21/EffluxLakes_selected_PLDLakes_2024-10-11.shp')
-    df = pd.read_excel('/Volumes/metis/ABOVE3/Tom/PrelimLakeMatchupData_2024-10-21.xlsx')
-    return gdf.merge(df, on='lake_id')
+    gdf_jn_PLD = gpd.read_file('/Volumes/metis/ABOVE3/Tom/Selected_PLD_Lakes_2024-10-21/EffluxLakes_selected_PLDLakes_2024-10-11.shp')
+    df = pd.read_excel('/Volumes/metis/ABOVE3/Tom/PrelimLakeMatchupData_2024-10-21.xlsx', sheet_name='Measurements')
+    gdf = gdf_jn_PLD.merge(df, on='lake_id', how='inner') #, validate='1:1')
+    gdf = gdf.groupby('lake_id').first().reset_index() # hot fix to remove dups
+    gdf.crs = gdf_jn_PLD.crs
+    return gdf
+
+
+def loadWBD():
+    '''Note: bbox is for AK'''
+    return gpd.read_file('/Volumes/thebe/Other/Feng-High-res-inland-surface-water-tundra-boreal-NA/edk_out/fixed_geoms/WBD.shp', engine='pyogrio', bbox = (-170, 51, -125 , 72))
+
 
 # gdf = loadEffluxShp()
+# gdf = loadWBD()
 # pass
---
+++ land_cover/plotting.py
@@ -0,0 +1,84 @@
+import geopandas as gpd
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io import img_tiles
+from matplotlib import pyplot as plt
+from pyproj.crs.crs import CRS
+
+def _crs2ccrs(crs):
+    epsg_code = crs.to_epsg()
+    # epsg_code = gdf.crs.to_epsg()  # Extract EPSG code
+    if epsg_code:
+        ccrs_projection = ccrs.epsg(epsg_code)
+    else:
+        if crs.to_dict()['proj'] == 'aea':
+            ccrs_projection = _cartopy_albers(crs)
+        elif crs.name == 'WGS 84':
+            ccrs_projection = ccrs.PlateCarree()
+        else:
+            raise ValueError(f"Mapping crs not supported: {crs.name}")
+    return ccrs_projection
+
+
+def _cartopy_albers(crs):
+
+    # # Example parameters for Albers Equal Area (adjust based on your GeoDataFrame)
+    # central_longitude = crs['longitude'] if 'longitude' in crs else -96  # default example
+    # central_latitude = crs['latitude'] if 'latitude' in crs else 37.5  # default example
+    # std_parallel_1 = crs['lat_1'] if 'lat_1' in crs else 29.5  # example standard parallel 1
+    # std_parallel_2 = crs['lat_2'] if 'lat_2' in crs else 45.5  # example standard parallel 2
+
+    # # Create the Cartopy Albers Equal Area CRS
+    # ccrs_projection = ccrs.AlbersEqualArea(
+    #     central_longitude=central_longitude,
+    #     central_latitude=central_latitude,
+    #     standard_parallels=(std_parallel_1, std_parallel_2)
+    # )
+
+    # Example parameters for Albers Equal Area (adjust based on your GeoDataFrame)
+    crs_dict = crs.to_dict()
+    central_longitude = crs_dict['lon_0'] 
+    central_latitude = crs_dict['lat_0'] 
+    std_parallel_1 = crs_dict['lat_1'] 
+    std_parallel_2 = crs_dict['lat_2'] 
+
+    # Create the Cartopy Albers Equal Area CRS
+    ccrs_projection = ccrs.AlbersEqualArea(
+        central_longitude=central_longitude,
+        central_latitude=central_latitude,
+        standard_parallels=(std_parallel_1, std_parallel_2)
+    )
+    return ccrs_projection
+
+
+def plot_basemap(gdf:gpd.GeoDataFrame, crs:CRS=None, color='red', zoom=6, alpha=0.7, **kwargs):
+    # set default
+    if crs is None:
+        crs = CRS.from_authority("ESRI", 102001)
+
+    ccrs_for_map = _crs2ccrs(crs)
+    ccrs_of_gdf = _crs2ccrs(gdf.crs)
+    
+    # Set up the plot with a specific Cartopy CRS for Alaska
+    fig, ax = plt.subplots(
+        figsize=(7, 10), 
+        subplot_kw={
+        # 'projection': 'ESRI:102001'})
+        'projection': ccrs_for_map}) # TODO: error here. The following works: 'projection': ccrs.AlbersEqualArea(central_longitude=-152, central_latitude=63)}
+            # 'projection': ccrs.AlbersEqualArea(central_longitude=-96, central_latitude=40)})
+
+    # Zoom into Alaska
+    # ax.set_extent([-170, -130, 54, 72], crs=ccrs.PlateCarree()) # TODO: auto read
+
+    # Add Google Satellite imagery
+    ax.add_image(img_tiles.GoogleTiles(style='satellite'), zoom)
+
+    # Add state outlines using Cartopy's features
+    ax.add_feature(cfeature.STATES.with_scale('110m'), edgecolor='white')
+
+    # Plot the GeoDataFrames
+    gdf.plot(ax=ax, color=color, markersize=5,
+                    transform=ccrs_of_gdf, alpha=alpha, **kwargs) #, label='Efflux lakes')
+
+    # ax.legend(title='Legend', loc='upper right')
+    plt.show()
