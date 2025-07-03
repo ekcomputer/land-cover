23bfdeb82b1328b08c49c70fc96c80612fcd2a24
ekcomputer <ethan_kyzivat@fas.harvard.edu>
2025-07-03 14:44:51 -0400

    batch nominatim geocoder

--- land_cover/geocode.py
+++ land_cover/geocode.py
@@ -1,12 +1,16 @@
 import time
 
+import geopandas as gpd
 import numpy as np
 import pandas as pd
+import pyproj
 import requests
 from geopy.distance import geodesic
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
 from shapely.geometry import Point, shape
+from shapely.ops import transform
+from tqdm import tqdm
 
 USER_AGENT = "edk"
 NOMINATIM_URL = "https://nominatim.openstreetmap.org/search"
@@ -44,29 +48,34 @@ def verified_polygon(
         raise ValueError("Only Nominatim service is supported.")
 
     results = polygons_nominatim(name, limit=limit)
-
-    for item in results:
-        geojson = item.get("geojson") if "geojson" in item else None
-        display_name = item.get("display_name")
-        lat = float(item.get("lat", np.nan)) if item.get("lat") else np.nan
-        lon = float(item.get("lon", np.nan)) if item.get("lon") else np.nan
-        # Get centroid of geojson polygon
-        if geojson and "coordinates" in geojson:
-            try:
-                # polygon = shape(geojson)
+    if results is not None:
+        for item in results:
+            geojson = item.get("geojson") if "geojson" in item else None
+            display_name = item.get("display_name")
+            lat = float(item.get("lat", np.nan)) if item.get("lat") else np.nan
+            lon = float(item.get("lon", np.nan)) if item.get("lon") else np.nan
+            place_id = item.get("place_id") if item.get("place_id") else np.nan
+            osm_id = item.get("osm_id") if item.get("osm_id") else np.nan
+            # Get centroid of geojson polygon
+            if geojson and "coordinates" in geojson:
+                try:
+                    polygon = shape(geojson)
+                    if lat_lon is None:
+                        # If no lat_lon provided, don't verify coordinates
+                        return geojson, display_name, lat, lon, place_id, osm_id
+                    dist_km = nearest_distance(lat_lon, polygon)
+                    if dist_km <= within:
+                        return polygon, display_name, lat, lon, place_id, osm_id
+                except Exception:
+                    continue
+            else:
+                # if only points are returned (need to verify this is possible)
                 if lat_lon is None:
-                    # If no lat_lon provided, don't verify coordinates
-                    return geojson, display_name, lat, lon
-                # centroid = polygon.centroid
+                    return None, display_name, lat, lon, place_id, osm_id
                 dist_km = geodesic((lat_lon[0], lat_lon[1]), (item["lat"], item["lon"])).km
                 if dist_km <= within:
-                    return geojson, display_name, lat, lon
-            except Exception:
-                continue
-        else:
-            # if only points are returned (need to verify this is possible)
-            return geojson, display_name, lat, lon
-    return [np.nan] * 4
+                    return geojson, display_name, lat, lon, place_id, osm_id
+    return None, np.nan, np.nan, np.nan, np.nan, np.nan
 
 
 # TODO: if no polygon, return location
@@ -84,39 +93,71 @@ def geonames_search(lake_name):
     return response.json().get("geonames", [])
 
 
-def within_10km(row):
-    try:
-        point = Point(row["long (decimal)"], row["lat (decimal)"])
-        polygon = shape(row["lake_geojson"])
-        return point.distance(polygon) <= 0.1  # ~10 km in degrees
-    except:
-        return False
+def nearest_distance(lat_lon, polygon):
+    """
+    Find the nearest distance between appoints and polygon, assumed to both be in WGS84
+    coordinates, using a local conformal projection.
+    """
+    # Calculate minimum distance from lat_lon to polygon bounds
+    point = Point(lat_lon[1], lat_lon[0])  # shapely uses (lon, lat)
+    # Project to a local Azimuthal equidistan conformal projection centered on lat_lon
+    proj_str = f"+proj=aeqd +lat_0={lat_lon[0]} +lon_0={lat_lon[1]} +datum=WGS84 +units=m +no_defs"
+    projection_transformer = pyproj.Transformer.from_crs(
+        "EPSG:4326", proj_str, always_xy=True
+    ).transform
+
+    polygon_proj = transform(projection_transformer, polygon)
+    point_proj = transform(projection_transformer, point)
+    return polygon_proj.boundary.distance(point_proj) / 1000  # meters to km
 
 
 if __name__ == "__main__":
     df = pd.read_csv("/Volumes/metis/ABOVE3/Bogard_suppl_data/edk_out/Bogard19_ESM_alldata_wh.csv")
-
-    geojson_list, names, lats, lons = [], []
-    for idx, row in df[94:].iterrows():
+    # df = df[94:100]  # temp
+    geojson_list, names, lats, lons, place_ids, osm_ids = [], [], [], [], [], []
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
         lake_name = row["lake name provided"]
         lat_lon = [row["lat (decimal)"], row["long (decimal)"]]
         if lake_name is not np.nan:
-            geojson, display_name, lat, lon = verified_polygon(lake_name, lat_lon, limit=10)
+            geojson, display_name, lat, lon, place_id, osm_id = verified_polygon(
+                lake_name, lat_lon, within=5, limit=10
+            )
             geojson_list.append(geojson)
             names.append(display_name)
             lats.append(lat)
             lons.append(lon)
+            place_ids.append(place_id)
+            osm_ids.append(osm_id)
             time.sleep(1)
         else:
             names.append(np.nan)
             geojson_list.append(None)
             lats.append(np.nan)
             lons.append(np.nan)
+            place_ids.append(np.nan)
+            osm_ids.append(np.nan)
 
-    df["lake_geojson"] = geojson_list
-    df["lake_full_name"] = names
-    df = df[df["lake_geojson"].notnull()]
+    df["geometry"] = geojson_list
+    df["geocode_full_name"] = names
+    df["geocode_lat"] = lats
+    df["geocode_lon"] = lons
+    df["geocode_place_id"] = place_ids
+    df["geocode_osm_id"] = osm_ids
+    df_filtered = df[df["geometry"].notnull()]
+    # Create GeoDataFrame from filtered DataFrame
+    gdf = gpd.GeoDataFrame(df, crs="EPSG:4326")
 
-    df["within_10km"] = df.apply(within_10km, axis=1)
-    df_filtered = df[df["within_10km"]]
-    df_filtered.to_csv("lakes_within_10km.csv", index=False)
+    # # Define Lambert azimuthal equal-area projection centered at given lat/lon
+    # laea_proj = (
+    #     f"+proj=laea +lat_0=64.95 +lon_0=-89.65 +datum=WGS84 +units=m +no_defs"
+    # )
+
+    gdf = gdf.to_crs("ESRI:102001")  # Canada Albers Equal Area Conic projection
+
+    # Write to shapefile
+    gdf.to_file(
+        "/Volumes/metis/ABOVE3/Bogard_suppl_data/edk_out/shp/Bogard19_ESM_alldata_wh_geocoded.gpkg"
+    )
+    # df_filtered.to_csv(
+    #     "/Volumes/metis/ABOVE3/Bogard_suppl_data/edk_out/Bogard19_ESM_alldata_wh_geocoded.csv", index=False
+    # )
