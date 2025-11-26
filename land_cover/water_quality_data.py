6423acaca4b2ade3b8e03976b2a618d19622483c
ekcomputer <ethan_kyzivat@fas.harvard.edu>
2025-11-25 19:02:35 -0500

    NEON veg datasets

--- land_cover/water_quality_data.py
+++ land_cover/water_quality_data.py
@@ -9,13 +9,22 @@ NEON_AK_CHEM_SURFACE_DIR_ZIP = "/Volumes/metis/ABOVE3/NEON/NEON_chem-surfacewate
 NEON_AK_WQ_DIR = "/Volumes/metis/ABOVE3/NEON/NEON_water-quality"
 NEON_AK_WQ_DIR_ZIP = "/Volumes/metis/ABOVE3/NEON/NEON_water-quality.zip"
 
+# Toolik lake 2017-2023
+NEON_AK_RIP_STRUCT_DIR = "/Volumes/metis/ABOVE3/NEON/NEON_comp-struct-riparian"
+NEON_AK_RIP_STRUCT_DIR_ZIP = "/Volumes/metis/ABOVE3/NEON/NEON_comp-struct-riparian.zip"
+
+# Only rivers
+NEON_AK_RIP_COVER_DIR = "/Volumes/metis/ABOVE3/NEON/NEON_cover-veg-riparian"
+NEON_AK_RIP_COVER_DIR_ZIP = "/Volumes/metis/ABOVE3/NEON/NEON_cover-veg-riparian.zip"
+
 
 NEON_OUTPUT_DIR = "/Volumes/metis/ABOVE3/NEON/edk_out"
 
 # Just run once!
 # nu.stack_by_table(NEON_AK_CHEM_SURFACE_DIR_ZIP)
-
-nu.stack_by_table(NEON_AK_WQ_DIR_ZIP)
+# nu.stack_by_table(NEON_AK_WQ_DIR_ZIP)
+nu.stack_by_table(NEON_AK_RIP_STRUCT_DIR_ZIP)
+nu.stack_by_table(NEON_AK_RIP_COVER_DIR_ZIP)
 
 # def load_NEON_chem_surface():
 #     return pd.read_csv(os.path.join(NEON_AK_DIR, "stackedFiles/swc_externalLabDataByAnalyte.csv"))
